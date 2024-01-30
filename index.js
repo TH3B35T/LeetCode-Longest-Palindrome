eba82f1badbfbdea94dfe3c6062f60728282357b
Yusuf Mehmet <107208399+TH3B35T@users.noreply.github.com>
2024-01-30 10:47:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,29 +4,45 @@
  */
 var longestPalindrome = function(s) {
 
+    // if(s.length <= 1){
+    //     return s.length;
+    // }
+
+    // function Counter(str){
+    //     const counter = new Map();
+    //     for(let char of str){
+    //         counter.set(char, (counter.get(char) || 0) + 1)
+    //     }
+    //     return counter;
+    // }
+    // const s_counter = Counter(s);
+    // let longest_plaindrom_len = 0;
+    // let is_odd_count = false;
+
+    // for(let count of s_counter.values()){
+    //     if(count % 2 == 0){
+    //         longest_plaindrom_len += count;
+    //     } else {
+    //         is_odd_count = true;
+    //         longest_plaindrom_len += count - 1;
+    //     }
+    // }
+    // // Add one odd character to the center
+    // return is_odd_count? longest_plaindrom_len += 1 : longest_plaindrom_len;
+
     if(s.length <= 1){
         return s.length;
     }
-
-    function Counter(str){
-        const counter = new Map();
-        for(let char of str){
-            counter.set(char, (counter.get(char) || 0) + 1)
-        }
-        return counter;
-    }
-    const s_counter = Counter(s);
+    let seen = new Set();
     let longest_plaindrom_len = 0;
-    let is_odd_count = false;
 
-    for(let count of s_counter.values()){
-        if(count % 2 == 0){
-            longest_plaindrom_len += count;
+    for(let char of s){
+        if(seen.has(char)){
+            longest_plaindrom_len +=2;
+            seen.delete(char);
         } else {
-            is_odd_count = true;
-            longest_plaindrom_len += count - 1;
+            seen.add(char);
         }
     }
-
-    return is_odd_count? longest_plaindrom_len += 1 : longest_plaindrom_len;
+    return seen.size > 0 ? longest_plaindrom_len + 1 : longest_plaindrom_len;
 };
